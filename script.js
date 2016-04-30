3ba21168b0471bb16a4eeac1a50481ca2cb2d5fa
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2016-04-29 22:31:31 -0600

    add more suitable names to vars and funcs

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
   makeExcuse();
 
   function makeExcuse() {
-    var quotes = ["I was confused about what line I was on!", "I lost my GPS signal..", "\
+    var excuses = ["I was confused about what line I was on!", "I lost my GPS signal..", "\
     What was I supposed to do there was a turkey on the tracks---!", "I hit a small \
     child and got a flat tire?","Damsel in Distress!!!!", "Too much junk in the trunk!", "\
      But.. it's 420", "Loose Caboose!", "I was literally struck by lightning.", " \
@@ -10,17 +10,17 @@ $(document).ready(function() {
       Re-routed to Hawaii", "Dazed and Confused", "I swear I saw a squirrel", "LANDSHARKS!", "\
       I was sexting the conductor", "I was going off the rails on a crazy train", "The city \
       of New Orleans"];
-    randomQuote = quotes[Math.floor(Math.random()*quotes.length)].split(',');
-    $('.saying').text(randomQuote[0]);
+    randomExcuse = excuses[Math.floor(Math.random()*excuses.length)].split(',');
+    $('.saying').text(randomExcuse[0]);
     }
 
     $(".button").click(function() {
       makeExcuse();
     });
 
-    var currentQuote = randomQuote[0] + "   - Annonymous";
+    var currentExcuse = randomExcuse[0] + "   - Annonymous";
 
     $("#twitterBtn").click(function() {
-      window.open("https://twitter.com/intent/tweet?text=" + currentQuote);
+      window.open("https://twitter.com/intent/tweet?text=" + currentExcuse);
     });
 });
